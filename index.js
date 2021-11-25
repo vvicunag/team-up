f31513e65595898dcafcc89dd81d3cebef943564
vvicunag <vvicuna1@uc.cl>
2021-11-25 17:32:15 -0300

    add inquirer functionality for engineer, manager, and intern

---
+++ index.js
@@ -0,0 +1,150 @@
+const inquirer = require("inquirer"); 
+const Employee = require("./lib/employee");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
+const engineers = [];
+const interns = [];
+const managers = [];
+
+
+//Question bank
+const initialQuestion = 
+[
+    {
+        type: 'list',
+        message: 'Do you want to add an employee?',
+        choices: ['Yes', 'No'],
+        name: 'addEmployee',
+    },  
+]
+
+const regularQuestions =
+[
+    {
+        type: 'input',
+        message: "What is the employee's name?",
+        name: 'employeeName',
+    },
+    {
+        type: 'input',
+        message: "What is the employee's id?",
+        name: 'employeeID',
+    },
+    {
+        type: 'input',
+        message: "What is the employee's email?",
+        name: 'employeeEmail',
+    }, 
+    {
+        type: 'list',
+        message: 'What role will this employee have?',
+        choices: ['Intern', 'Engineer', 'Manager'],
+        name: 'employeeRole',
+    },     
+]
+
+const initialize = () => {
+    inquirer.prompt(initialQuestion).then((response) => {
+        if(response.addEmployee === "Yes") {
+           inquirer.prompt(regularQuestions).then((response) => {
+                const employeeData = response;
+                if(employeeData.employeeRole === "Engineer") {
+                    getEngineerData(employeeData);
+                }
+                else if(employeeData.employeeRole === "Student") {
+                    getInternData(employeeData);
+                }
+                else {
+                    getManagerData(employeeData);
+                }
+           }
+        )};
+    }
+)};
+
+getEngineerData = (employeeData) => {
+    inquirer.prompt({
+        type: 'input',
+        message: 'What is the GitHub of this engineer?',
+        name: 'engineerGithub',
+    }).then((response) => {
+        console.log(response);
+        employeeData = {
+            ...employeeData,
+            ...response
+        }
+        console.log(employeeData);
+        generateEngineer(employeeData);
+    })
+}
+
+generateEngineer = (employeeData) => {
+    engineers.push(new Engineer(
+        employeeData.employeeName,
+        employeeData.employeeID, 
+        employeeData.employeeEmail, 
+        employeeData.engineerGithub
+    )) 
+    console.log(engineers);
+    initialize();
+}
+
+getInternData = (employeeData) => {
+    inquirer.prompt({
+        type: 'input',
+        message: 'What is the school of this intern?',
+        name: 'internSchool',
+    }).then((response) => {
+        console.log(response);
+        employeeData = {
+            ...employeeData,
+            ...response
+        }
+        console.log(employeeData);
+        generateIntern(employeeData);
+    })
+}
+
+generateIntern = (employeeData) => {
+    interns.push(new Intern(
+        employeeData.employeeName,
+        employeeData.employeeID, 
+        employeeData.employeeEmail, 
+        employeeData.internSchool,
+    )) 
+    console.log(interns);
+    initialize();
+}
+
+getManagerData = (employeeData) => {
+    inquirer.prompt({
+        type: 'input',
+        message: 'What is the office number of this manager?',
+        name: 'managerOfficeNumber',
+    }).then((response) => {
+        console.log(response);
+        employeeData = {
+            ...employeeData,
+            ...response
+        }
+        console.log(employeeData);
+        generateManager(employeeData);
+    })
+}
+
+generateManager = (employeeData) => {
+    managers.push(new Manager(
+        employeeData.employeeName,
+        employeeData.employeeID, 
+        employeeData.employeeEmail, 
+        employeeData.managerOfficeNumber
+    )) 
+    console.log(managers);
+    initialize();
+}
+
+
+// Initialize app
+initialize();
+
