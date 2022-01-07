5c140d0459b903c6a31ffcb25213066f6a94577c
vvicunag <vvicuna1@uc.cl>
2022-01-07 14:09:51 +0100

    erase console logs and add README

--- dist/render-profiles.js
+++ dist/render-profiles.js
@@ -1,10 +1,9 @@
 const fs = require("fs");
 const $ = require("jquery");
-const {engineers} = require("../index");
-const Engineer = require("../lib/engineer.js")
+const { engineers } = require("../index");
+const Engineer = require("../lib/engineer.js");
 
-let html = 
-`<!DOCTYPE html>
+let html = `<!DOCTYPE html>
 <html>
     <head>
         <html lang="en"></html>
@@ -14,9 +13,8 @@ let html =
     </head>
     <body>
         <h1>Team Up!</h1>
-<main>`
-let html2 = 
-`<script
+<main>`;
+let html2 = `<script
 src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
 integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
 crossorigin="anonymous"
@@ -24,12 +22,11 @@ referrerpolicy="no-referrer"
 ></script>
 <script src="./render-profiles.js"></script>
 </body>
-</html>`
+</html>`;
 
 const generateHTML = (engineers, interns, managers) => {
-    managers.map((manager) => {
-        let managerCard = 
-        `<div class = "card-container">
+  managers.map((manager) => {
+    let managerCard = `<div class = "card-container">
         <div class = "card-header">
         <h2 class = "card-title">Manager</h2>
         <h2 class = "card-title">${manager.name}</h2>
@@ -38,13 +35,11 @@ const generateHTML = (engineers, interns, managers) => {
         <p class = card-detail>ID: ${manager.id}</p>
         <p class = card-detail>Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
         <p class = card-detail>Office number: ${manager.officeNumber}</p>        </div>
-        </div>`
-        html = html + managerCard;
-    });
-    engineers.map((engineer) => {
-        console.log("My engineer", engineer);
-        let engineerCard = 
-        `<div class = "card-container">
+        </div>`;
+    html = html + managerCard;
+  });
+  engineers.map((engineer) => {
+    let engineerCard = `<div class = "card-container">
         <div class = "card-header">
         <h2 class = "card-title">Engineer</h2>
         <h2 class = "card-title">${engineer.name}</h2>
@@ -53,12 +48,11 @@ const generateHTML = (engineers, interns, managers) => {
         <p class = card-detail>ID: ${engineer.id}</p>
         <p class = card-detail>Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
         <p class = card-detail>Github: <a href="https://github.com/${engineer.gitHub}">${engineer.gitHub}</a></p>        </div>
-        </div>`
-        html = html + engineerCard;
-    });
-    interns.map((intern) => {
-        let internCard = 
-        `<div class = "card-container">
+        </div>`;
+    html = html + engineerCard;
+  });
+  interns.map((intern) => {
+    let internCard = `<div class = "card-container">
         <div class = "card-header">
         <h2 class = "card-title">Intern</h2>
         <h2 class = "card-title">${intern.name}</h2>
@@ -67,15 +61,17 @@ const generateHTML = (engineers, interns, managers) => {
         <p class = card-detail>ID: ${intern.id}</p>
         <p class = card-detail>Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
         <p class = card-detail>School: ${intern.school}</p>        </div>
-        </div>`
-        html = html + internCard;
-    });
+        </div>`;
+    html = html + internCard;
+  });
 
+  html = html + html2;
 
-    html = html + html2;
-
-
-    fs.writeFileSync("./generate.html", html, (error) => 
-    error ? console.error(error) : console.info('cahapa la pachala!'));
-};     
+  fs.writeFileSync("./generate.html", html, (error) =>
+    error ? console.error(error) : console.info("")
+  );
+  console.log(
+    "Successfully generated HTML! Look for 'generate.html' in file directory"
+  );
+};
 module.exports = generateHTML;
--- index.js
+++ index.js
@@ -1,157 +1,152 @@
-const inquirer = require("inquirer"); 
+const inquirer = require("inquirer");
 const fs = require("fs");
 const Employee = require("./lib/employee");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
-const generateHTML = require("./dist/render-profiles"); 
+const generateHTML = require("./dist/render-profiles");
 const engineers = [];
 const interns = [];
 const managers = [];
 
-
 //Question bank
-const initialQuestion = 
-[
-    {
-        type: 'list',
-        message: 'Do you want to add an employee?',
-        choices: ['Yes', 'No'],
-        name: 'addEmployee',
-    },  
-]
+const initialQuestion = [
+  {
+    type: "list",
+    message: "Do you want to add an employee?",
+    choices: ["Yes", "No"],
+    name: "addEmployee",
+  },
+];
 
-const regularQuestions =
-[
-    {
-        type: 'input',
-        message: "What is the employee's name?",
-        name: 'employeeName',
-    },
-    {
-        type: 'input',
-        message: "What is the employee's id?",
-        name: 'employeeID',
-    },
-    {
-        type: 'input',
-        message: "What is the employee's email?",
-        name: 'employeeEmail',
-    }, 
-    {
-        type: 'list',
-        message: 'What role will this employee have?',
-        choices: ['Intern', 'Engineer', 'Manager'],
-        name: 'employeeRole',
-    },     
-]
+const regularQuestions = [
+  {
+    type: "input",
+    message: "What is the employee's name?",
+    name: "employeeName",
+  },
+  {
+    type: "input",
+    message: "What is the employee's id?",
+    name: "employeeID",
+  },
+  {
+    type: "input",
+    message: "What is the employee's email?",
+    name: "employeeEmail",
+  },
+  {
+    type: "list",
+    message: "What role will this employee have?",
+    choices: ["Intern", "Engineer", "Manager"],
+    name: "employeeRole",
+  },
+];
 
 const initialize = () => {
-    inquirer.prompt(initialQuestion).then((response) => {
-        if(response.addEmployee === "Yes") {
-           inquirer.prompt(regularQuestions).then((response) => {
-                const employeeData = response;
-                if(employeeData.employeeRole === "Engineer") {
-                    getEngineerData(employeeData);
-                }
-                else if(employeeData.employeeRole === "Intern") {
-                    getInternData(employeeData);
-                }
-                else {
-                    getManagerData(employeeData);
-                }
-           }
-        )} 
-        else {
-            generateHTML(engineers, interns, managers)
+  inquirer.prompt(initialQuestion).then((response) => {
+    if (response.addEmployee === "Yes") {
+      inquirer.prompt(regularQuestions).then((response) => {
+        const employeeData = response;
+        if (employeeData.employeeRole === "Engineer") {
+          getEngineerData(employeeData);
+        } else if (employeeData.employeeRole === "Intern") {
+          getInternData(employeeData);
+        } else {
+          getManagerData(employeeData);
         }
-    }   
-)};
+      });
+    } else {
+      generateHTML(engineers, interns, managers);
+    }
+  });
+};
 
 getEngineerData = (employeeData) => {
-    inquirer.prompt({
-        type: 'input',
-        message: 'What is the GitHub of this engineer?',
-        name: 'engineerGithub',
-    }).then((response) => {
-        console.log(response);
-        employeeData = {
-            ...employeeData,
-            ...response
-        }
-        console.log(employeeData);
-        generateEngineer(employeeData);
+  inquirer
+    .prompt({
+      type: "input",
+      message: "What is the GitHub of this engineer?",
+      name: "engineerGithub",
     })
-}
+    .then((response) => {
+      employeeData = {
+        ...employeeData,
+        ...response,
+      };
+      generateEngineer(employeeData);
+    });
+};
 
 generateEngineer = (employeeData) => {
-    engineers.push(new Engineer(
-        employeeData.employeeName,
-        employeeData.employeeID, 
-        employeeData.employeeEmail, 
-        employeeData.engineerGithub
-    )) 
-    console.log(engineers);
-    initialize();
-}
+  engineers.push(
+    new Engineer(
+      employeeData.employeeName,
+      employeeData.employeeID,
+      employeeData.employeeEmail,
+      employeeData.engineerGithub
+    )
+  );
+  initialize();
+};
 
 getInternData = (employeeData) => {
-    inquirer.prompt({
-        type: 'input',
-        message: 'What is the school of this intern?',
-        name: 'internSchool',
-    }).then((response) => {
-        console.log(response);
-        employeeData = {
-            ...employeeData,
-            ...response
-        }
-        console.log(employeeData);
-        generateIntern(employeeData);
+  inquirer
+    .prompt({
+      type: "input",
+      message: "What is the school of this intern?",
+      name: "internSchool",
     })
-}
+    .then((response) => {
+      employeeData = {
+        ...employeeData,
+        ...response,
+      };
+      generateIntern(employeeData);
+    });
+};
 
 generateIntern = (employeeData) => {
-    interns.push(new Intern(
-        employeeData.employeeName,
-        employeeData.employeeID, 
-        employeeData.employeeEmail, 
-        employeeData.internSchool,
-    )) 
-    console.log(interns);
-    initialize();
-}
+  interns.push(
+    new Intern(
+      employeeData.employeeName,
+      employeeData.employeeID,
+      employeeData.employeeEmail,
+      employeeData.internSchool
+    )
+  );
+  initialize();
+};
 
 getManagerData = (employeeData) => {
-    inquirer.prompt({
-        type: 'input',
-        message: 'What is the office number of this manager?',
-        name: 'managerOfficeNumber',
-    }).then((response) => {
-        console.log(response);
-        employeeData = {
-            ...employeeData,
-            ...response
-        }
-        console.log(employeeData);
-        generateManager(employeeData);
+  inquirer
+    .prompt({
+      type: "input",
+      message: "What is the office number of this manager?",
+      name: "managerOfficeNumber",
     })
-}
+    .then((response) => {
+      employeeData = {
+        ...employeeData,
+        ...response,
+      };
+      generateManager(employeeData);
+    });
+};
 
 generateManager = (employeeData) => {
-    managers.push(new Manager(
-        employeeData.employeeName,
-        employeeData.employeeID, 
-        employeeData.employeeEmail, 
-        employeeData.managerOfficeNumber
-    )) 
-    console.log(managers);
-    initialize();
-}
-
+  managers.push(
+    new Manager(
+      employeeData.employeeName,
+      employeeData.employeeID,
+      employeeData.employeeEmail,
+      employeeData.managerOfficeNumber
+    )
+  );
+  initialize();
+};
 
 // Initialize app
 initialize();
 
-
-module.exports = {engineers, interns, managers}; 
+module.exports = { engineers, interns, managers };
